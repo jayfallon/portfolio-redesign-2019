ac582cbf645d25c5d9da7f0e9bd3b2e990a615e7
Jay Fallon <jayfallon@gmail.com>
2019-08-08 13:08:34 -0400

    corrected name, added eslint

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,15 @@ import PageTitle from "../components/PageTitle";
 
 const Index = ({ posts }) => (
   <>
-    <PageTitle>Hello, world.</PageTitle>
+    <PageTitle>Hello, worlds.</PageTitle>
     <p>
       Next.js PWA Boilerplate starts your progressive web app off with a perfect
       Google Lighthouse score. To see this in action, open this website with
       Chrome, go to the “Audits” tab in dev tools, and click “Run audit”.
     </p>
+    <ul>
+      <li>Testing eslint</li>
+    </ul>
     <p>
       Check out the{" "}
       <a
