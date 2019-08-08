17f0247ef1634e3b16f4cc9833dd03bd95f640f6
Jay Fallon <jayfallon@gmail.com>
2019-08-08 17:02:37 -0400

    stubbed out footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,14 +1,87 @@
 import Link from 'next/link';
+import * as moment from 'moment';
+
+import FooterStyles from './styles';
+
+const now = moment().format('YYYY');
+
+const myEmail = 'jayfallon@gmail.com';
+const myName = 'Jay Fallon';
+
+const footerPNav = [
+  {
+    title: 'LinkedIn profile page - opens in separate window',
+    url: 'https://www.linkedin.com/in/jayfallon/',
+    text: 'LinkedIn',
+  },
+  {
+    title: 'GitHub profile page - opens in separate window',
+    url: 'https://github.com/jayfallon',
+    text: 'GitHub',
+  },
+  {
+    title: 'Medium profile page - opens in separate window',
+    url: 'https://medium.com/@jayfallon',
+    text: 'Medium',
+  },
+  {
+    title: 'Instagram profile page - opens in separate window',
+    url: 'https://www.instagram.com/jayfallon/',
+    text: 'Instagram',
+  },
+  {
+    title: 'Resume page link',
+    url: '/resume',
+    text: 'Resume',
+  },
+];
+
+const footerSNav = [
+  {
+    title: 'Privacy page link',
+    url: '/privacy',
+    text: 'Privacy',
+  },
+  {
+    title: 'Terms & Conditions page link',
+    url: '/terms',
+    text: 'Terms',
+  },
+];
 
 const Footer = () => (
-  <footer>
-    <h1>this is the footer</h1>
-    <p className="footer__link--top">
-      <a title="hello" href="#main__content">
-        Back to top
-      </a>
-    </p>
-  </footer>
+  <FooterStyles>
+    <section>
+      <h3>Let's talk</h3>
+      <p>{myEmail}</p>
+      <nav>
+        <ul className="footer__nav--list-primary">
+          {footerPNav.map((elem, i) => (
+            <li key={i}>
+              <Link href={elem.url}>
+                <a title={elem.title}>{elem.text}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <ul className="footer__nav--list-secondary">
+          <li>
+            &copy;{now} - {myName}
+          </li>
+          {footerSNav.map((elem, i) => (
+            <li>
+              <Link href={elem.url}>
+                <a title={elem.title}>{elem.text}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <Link href="#main__content">
+        <a title="hello">Back to top</a>
+      </Link>
+    </section>
+  </FooterStyles>
 );
 
 export default Footer;
